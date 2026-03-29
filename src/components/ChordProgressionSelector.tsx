b657b27b4aaec032b3e17611e9c6f087a85f2016
Aleks Lovestech <alekslovestech@gmail.com>
2026-03-29 19:14:49 +0200

    remove early return

--- src/components/ChordProgressionSelector.tsx
+++ src/components/ChordProgressionSelector.tsx
@@ -10,44 +10,50 @@ import { ChordProgressionFormatter } from "@/utils/formatters/ChordProgressionFo
 export const ChordProgressionSelector = () => {
   const { selectedProgression, setSelectedProgression } = useAudio();
 
-  if (selectedProgression === null) return null;
-  const progression =
-    ChordProgressionLibrary.getProgression(selectedProgression);
   const chordSequenceText =
-    ChordProgressionFormatter.formatRomanNumeralsForDisplay(progression);
+    selectedProgression != null
+      ? ChordProgressionFormatter.formatRomanNumeralsForDisplay(
+          ChordProgressionLibrary.getProgression(selectedProgression),
+        )
+      : null;
 
   const handleChordProgressionChange = (
     event: React.ChangeEvent<HTMLSelectElement>,
   ) => {
-    const progression = event.target.value as ChordProgressionType;
-    setSelectedProgression(progression);
+    const value = event.target.value;
+    if (value === "") {
+      setSelectedProgression(null);
+      return;
+    }
+    setSelectedProgression(value as ChordProgressionType);
     console.log(`handling chord progression change`);
   };
 
   return (
     <div className="chord-progression-selector text-sm font-medium max-w-[80%]">
-      {
-        <div className="flex flex-col gap-2">
-          <Select
-            id="chord-progression-select"
-            value={selectedProgression ?? ""}
-            onChange={handleChordProgressionChange}
-            title="Select chord progression"
-            className="w-full max-w-full"
-          >
-            {Object.values(ChordProgressionType).map((mode) => (
-              <option
-                id={`chord-progression-option-${mode}`}
-                key={mode}
-                value={mode}
-              >
-                {mode}
-              </option>
-            ))}
-          </Select>
-          {chordSequenceText != null ? <div>{chordSequenceText}</div> : null}
-        </div>
-      }
+      <div className="flex flex-col gap-2">
+        <Select
+          id="chord-progression-select"
+          value={selectedProgression ?? ""}
+          onChange={handleChordProgressionChange}
+          title="Select chord progression"
+          className="w-full max-w-full"
+        >
+          <option value="" disabled>
+            Select chord progression
+          </option>
+          {Object.values(ChordProgressionType).map((mode) => (
+            <option
+              id={`chord-progression-option-${mode}`}
+              key={mode}
+              value={mode}
+            >
+              {mode}
+            </option>
+          ))}
+        </Select>
+        {chordSequenceText != null ? <div>{chordSequenceText}</div> : null}
+      </div>
     </div>
   );
 };
